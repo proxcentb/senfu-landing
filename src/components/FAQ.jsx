64b2ce38129d1f9e41940ef77405e38fb22fe923
proxcentb <proxcentb@gmail.com>
2023-11-20 04:49:56 +0300

    various improvements

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -8,11 +8,27 @@ export default function FaqSection() {
     <div className="w-11/12 md:w-8/12 lg:w-6/12">
       <h2 className="text-3xl font-bold mb-8">Frequently asked questions</h2>
       <div className="space-y-4 mb-10">
-        <Accordion question='What services does Senfu offer?' answer='Senfu specializes in creating realistic visualizations of architectural projects by integrating advanced drone photography with precise 3D modeling. Our services include creation of accurate and immersive 360-degree panoramas and tailor-made drone missions for site photography.' />
-        <Accordion question='How do you ensure accuracy of the result?' answer='We correlate real-life GPS points with points in 3D model. When we have enough data, algorithm finds the best flight path and creates it.'/>
-        <Accordion question='Can you work with projects at any stage of development?' answer='Yes, however, results are better when there are no obstacles around.'/>
-        <Accordion question='What makes your visualization services unique?' answer='Our approach allows us to provide highly accurate, realistic, and immersive visualizations that traditional photography and 3D modeling alone cannot achieve.'/>
-        <Accordion question='How long does it take to complete a project?' answer="The timeline varies depending on the project's complexity and scope. Generally, a standard project involving 16 spherical panoramas can be completed within a few days after the drone shoot. We work closely with clients to meet their schedules and provide timely deliverables."/>
+        <Accordion question='What services does Senfu offer?'>
+          Senfu specializes in creating realistic visualizations of architectural projects by integrating 
+          advanced drone photography with precise 3D modeling. Our services include creation of 
+          accurate and immersive 360-degree panoramas and tailor-made drone missions for site photography
+        </Accordion>
+        <Accordion question='How do you ensure accuracy of the result?'>
+          We correlate real-life GPS points with points in 3D model. When we have enough data, 
+          algorithm finds the best flight path and creates it.
+        </Accordion>
+        <Accordion question='Can you work with projects at any stage of development?'>
+          Yes, however, results are better when there are no obstacles around.
+        </Accordion>
+        <Accordion question='What makes your visualization services unique?'>
+          Our approach allows us to provide highly accurate, realistic, and immersive visualizations that 
+          traditional photography and 3D modeling alone cannot achieve.
+        </Accordion>
+        <Accordion question='How long does it take to complete a project?'>
+          The timeline varies depending on the project's complexity and scope. Generally, a standard project 
+          involving 16 spherical panoramas can be completed within a few days after the drone shoot. We work 
+          closely with clients to meet their schedules and provide timely deliverables.
+        </Accordion>
       </div>
       <h3 className="text-xl font-semibold">Still have questions?</h3>
       Feel free to reach out to us for any inquiries or questions you may have.
@@ -23,7 +39,7 @@ export default function FaqSection() {
   );
 }
 
-function Accordion({ question, answer }) {
+function Accordion({ question, children: answer }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'linear-gradient(to right, #364e57, #442432)',
-        'hero': "url('../../public/hero1.png')",
-      },
       colors: {
         'c1': '#374151',
         'c2': '#111827',
         'c3': '#F3F4F6',
-        'c4': '#E5E7EB',
       },
       height: {
         '80vh': '80vh',
